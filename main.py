ae1bfcfd716095dc3e03dc3e10a9c1ebc8357da6
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-04-30 16:14:14 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+microIoT.microIoT_initDisplay()
+wifi_name = "YOUR WIFI NAME HERE"
+password = "YOUR PASSWORD HERE"
+microIoT.microIoT_WIFI(wifi_name, password)
---
+++ main.ts
@@ -0,0 +1,4 @@
+microIoT.microIoT_initDisplay()
+let wifi_name = "YOUR WIFI NAME HERE"
+let password = "YOUR PASSWORD HERE"
+microIoT.microIoT_WIFI(wifi_name, password)
